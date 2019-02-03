d98f334117c911802adf8af588a16e060c121c0f
italo fontes <italo5123@hotmail.com>
2019-02-03 20:59:17 -0200

    Updates

---
+++ precache-manifest.210d9e2f8cc06d43a3e073bb75d34dd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15a7229ddc297f1780f1",
+    "url": "/static/css/main.23197e06.chunk.css"
+  },
+  {
+    "revision": "15a7229ddc297f1780f1",
+    "url": "/static/js/main.15a7229d.chunk.js"
+  },
+  {
+    "revision": "7bf23c13d127f682bfee",
+    "url": "/static/js/1.7bf23c13.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aa9c27f8f691884018cca8de4a6d82f6",
+    "url": "/index.html"
+  }
+];
